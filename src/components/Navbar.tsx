7e9113ad0adf4cac0713ad1ba14b33295dd39e91
Dylan Longlands <d.longlands99@gmail.com>
2022-05-06 21:34:54 +0100

    Inital Navbar setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+
+const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
+    const handleNav = () => setNav(!nav);
+
+    return (
+        <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+            <div className='px-2 flex justify-between items-center w-full h-full'>
+                <div className='flex items-center'>
+                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Brand.</h1>
+                    <ul className='hidden md:flex'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Support</li>
+                        <li>Platform</li>
+                        <li>Pricing</li>
+                    </ul>
+                </div>
+                <div className='hidden md:flex pr-4'>
+                    <button className='border-none bg-transparent text-black mr-4'>Sign In</button>
+                    <button className='px-8 py-3'>Sign Up</button>
+                </div>
+                <div className='flex md:hidden'  onClick={handleNav}>
+                    {!nav ? <MenuIcon className='w-5'/> : <XIcon className='w-5' />}
+                </div>
+            </div>
+
+            {nav && 
+            <ul className='absolute bg-zinc-200 w-full px-8 md:hidden'>
+                <li className='border-b-2 border-zinc-300 w-full'>Home</li>
+                <li className='border-b-2 border-zinc-300 w-full'>About</li>
+                <li className='border-b-2 border-zinc-300 w-full'>Support</li>
+                <li className='border-b-2 border-zinc-300 w-full'>Platform</li>
+                <li className='border-b-2 border-zinc-300 w-full'>Pricing</li>
+                <div className='flex flex-col my-4'>
+                    <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
+                    <button className='px-8 py-3'>Sign Up</button>
+                </div>
+            </ul>
+            }
+        </div>
+    )
+}
+
+export default Navbar;
